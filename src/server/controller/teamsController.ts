f172db449b59449dbffb5594a92c63b02d93f66a
Cynthia Wong <cynthialeewong@gmail.com>
2026-02-05 11:04:07 -0500

    fixed routes, controller

--- src/server/controller/teamsController.ts
+++ src/server/controller/teamsController.ts
@@ -14,12 +14,12 @@ const pool = new Pool({
 
 
 export default {
-  getTrustControls: async (_, res, next) => {
+  getTeams: async (_, res, next) => {
     try {
       const result = await pool.query('SELECT * FROM "allTeams"');
 
       if (!result) {
-        res.locals.dbResults = "No trust controller controller data";
+        res.locals.dbResults = "No teams controller data";
         return next();
       }
       res.locals.dbResults = result.rows;
@@ -29,10 +29,10 @@ export default {
       return next();
     } catch (error) {
       const serverError = {
-        log: `Error in Trust Controller middleware: ${error instanceof Error ? error.message : "Unknown error"}`,
+        log: `Error in Teams Controller middleware: ${error instanceof Error ? error.message : "Unknown error"}`,
         status: 500,
         message: {
-          err: "Failed to correctly retrieve the database  query for Trust Conrols",
+          err: "Failed to correctly retrieve the database  query for Teams Controls",
         },
       };
       return next(serverError);
--- src/server/router/router.ts
+++ src/server/router/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import faqController from "../controller/faqController";
 import trustController from "../controller/trustController";
 import allTeams from "../controller/teamsController";
+import teamsController from "../controller/teamsController";
 
 
 const router = express.Router();
@@ -15,15 +16,12 @@ router.get("/trustControls", trustController.getTrustControls, (_, res) => {
 });
 
 
-router.get("/allTeams", trustController.getTrustControls, (_, res) => {
+router.get("/allTeams", teamsController.getTeams, (_, res) => {
   return res.status(200).json(res.locals.dbResults);
 });
-
-
-
-
 
 router.get("/trustFaqs", faqController.getTrustFaqs, (_, res) => {
   return res.status(200).json(res.locals.dbResults);
 });
+
 export default router;
