eb1a4c89a027da08f358dc276a2de337997a10c1
Mrabdallaali <mrabdallaalii@gmail.com>
2026-02-13 09:37:09 -0500

    chatbot done

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AppComponent from "./components/client-components-dropdown";
 import allTrustControls from "../server/data/allTrustControls.json";
@@ -7,10 +7,9 @@ import Faqs from "./components/Faqs/Faqs";
 import SearchBar from "./components/Searchbar/SearchBar";
 import TrustCenter from "./components/TrustCenter/TrustCenter";
 import MultiSelect from "./components/Searchbar/MultiSelect";
-
+import Interface from "./Chatbot/Interface";
 
 export default function App() {
-
   // track selected categories - managed at App level
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
@@ -19,19 +18,19 @@ export default function App() {
     setSelectedCategories(categories);
   };
 
-
   return (
     <div className="trustFaqs_page">
       {/* <SearchBar /> */}
       <MultiSelect onFilterChange={handleFilterChange} />
 
-     <div className="trustFaqs_grid">        
-      <section className="trustFaqs_col" aria-label="Trust Center">
+      <div className="trustFaqs_grid">
+        <section className="trustFaqs_col" aria-label="Trust Center">
           <TrustCenter selectedCategories={selectedCategories} />
         </section>
         <section className="trustFaqs_col" aria-label="FAQs">
           <Faqs selectedCategories={selectedCategories} />
         </section>
+        <Interface />
       </div>
     </div>
   );
