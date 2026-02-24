e8b7be74d1a506fbd329742a48e2a085ba665937
Kevin <kevin.ortiz.software@gmail.com>
2026-02-23 19:39:20 -0500

    Changing AI model to moonshotai/kimi-k2-instruct-0905:groq, response time with 301 milliiseconds

--- src/server/envVariables.ts
+++ src/server/envVariables.ts
@@ -2,9 +2,14 @@ import "dotenv/config";
 
 // export const { PG_URI } = process.env;
 
-export const { PG_URI } = process.env
+export const { PG_URI } = process.env;
 export const { AI_APIKEY } = process.env;
 
-export const model  = "Qwen/Qwen2.5-Coder-7B-Instruct:nscale";
+// average response time from the http request is 4.8 seconds
+// export const model  = "Qwen/Qwen2.5-Coder-7B-Instruct:nscale";
 
-export const apiBase ="http://localhost:3000/api"
+// average response time from the http request is 1.6 seconds
+// export const model = "openai/gpt-oss-120b:groq";
+
+// average response time from the http request is 301 milliseconds
+export const model = "moonshotai/Kimi-K2-Instruct-0905:groq";
